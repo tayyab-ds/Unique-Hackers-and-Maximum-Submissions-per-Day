e59585c82c9964fffa6a00f95ef8406d8e7a0fed
tayyab.majeed <129239852+tayyab-ds@users.noreply.github.com>
2024-09-05 13:47:17 +0500

    Add files via upload

---
+++ Hackers_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Hackers (
+    HackerID INT,
+    Name VARCHAR(50)
+);
+
+INSERT INTO Hackers (HackerID, Name)
+VALUES 
+(15758, 'Rose'),
+(20703, 'Angela'),
+(36396, 'Frank'),
+(38289, 'Patrick'),
+(44065, 'Lisa'),
+(53473, 'Kimberly'),
+(62529, 'Bonnie'),
+(79722, 'Michael');
